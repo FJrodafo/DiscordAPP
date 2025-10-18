46713fffd2fc2bdcd9a36fdb0010ad45d8c6335f
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2025-10-18 18:50:04 +0100

    Update ready.js App/src

--- App/src/events/ready.js
+++ App/src/events/ready.js
@@ -8,12 +8,12 @@ module.exports = {
         console.log(`Server count: ${client.guilds.cache.size}`);
         const status = [
             { activities: [{ name: 'I respond to DMs', type: ActivityType.Custom }], status: 'online' },
-            { activities: [{ name: 'custom', type: ActivityType.Custom, state: '\u200B' }], status: 'online' },
-            { activities: [{ name: '...', type: ActivityType.Listening }], status: 'idle' },
-            { activities: [{ name: '...', type: ActivityType.Watching }], status: 'dnd' },
-            { activities: [{ name: '...', type: ActivityType.Playing }], status: 'idle' },
-            { activities: [{ name: '...', type: ActivityType.Competing }], status: 'dnd' },
-            { activities: [{ name: '...', type: ActivityType.Streaming, url: 'https://www.twitch.tv/directory' }] },
+            { activities: [{ name: 'custom', type: ActivityType.Custom, state: 'What\'s on your mind?' }], status: 'online' },
+            { activities: [{ name: 'music', type: ActivityType.Listening }], status: 'idle' },
+            { activities: [{ name: 'movies', type: ActivityType.Watching }], status: 'dnd' },
+            { activities: [{ name: 'video games', type: ActivityType.Playing }], status: 'idle' },
+            { activities: [{ name: 'video games', type: ActivityType.Competing }], status: 'dnd' },
+            { activities: [{ name: 'video games', type: ActivityType.Streaming, url: 'https://www.twitch.tv/directory' }] },
         ];
         function updateStatus() {
             const random = Math.floor(Math.random() * status.length);
