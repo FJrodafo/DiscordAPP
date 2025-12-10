40d8755129c7435d37081347fa5bedeef6908a98
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2025-12-10 12:10:32 +0000

    Typo ready.js

--- App/src/events/ready.js
+++ App/src/events/ready.js
@@ -9,11 +9,11 @@ module.exports = {
         const status = [
             { activities: [{ name: 'I respond to DMs', type: ActivityType.Custom }], status: 'online' },
             { activities: [{ name: 'custom', type: ActivityType.Custom, state: 'What\'s on your mind?' }], status: 'online' },
-            { activities: [{ name: 'music', type: ActivityType.Listening }], status: 'idle' },
-            { activities: [{ name: 'movies', type: ActivityType.Watching }], status: 'dnd' },
-            { activities: [{ name: 'video games', type: ActivityType.Playing }], status: 'idle' },
-            { activities: [{ name: 'video games', type: ActivityType.Competing }], status: 'dnd' },
-            { activities: [{ name: 'video games', type: ActivityType.Streaming, url: 'https://www.twitch.tv/directory' }] },
+            { activities: [{ name: 'Music', type: ActivityType.Listening }], status: 'idle' },
+            { activities: [{ name: 'Movies', type: ActivityType.Watching }], status: 'dnd' },
+            { activities: [{ name: 'Video Games', type: ActivityType.Playing }], status: 'idle' },
+            { activities: [{ name: 'Video Games', type: ActivityType.Competing }], status: 'dnd' },
+            { activities: [{ name: 'Video Games', type: ActivityType.Streaming, url: 'https://www.twitch.tv/directory' }] },
         ];
         function updateStatus() {
             const random = Math.floor(Math.random() * status.length);
