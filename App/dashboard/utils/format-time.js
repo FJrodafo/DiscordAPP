0603e44488f35198f281789f6b768e40815ab26d
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-02-07 13:05:09 +0000

    Upload Dashboard/utils

---
+++ App/dashboard/utils/format-time.js
@@ -0,0 +1,8 @@
+function formatTime(sec) {
+    const h = Math.floor(sec / 3600);
+    const m = Math.floor((sec % 3600) / 60);
+    const s = Math.floor(sec % 60);
+    return `${h}h ${m}m ${s}s`;
+}
+
+module.exports = { formatTime };
