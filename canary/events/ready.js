19e31b1cd6f65829c9f0763031678e7e81b2ece3
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-05-18 09:00:25 +0100

    Update ready.js (canary/events)

--- canary/events/ready.js
+++ canary/events/ready.js
@@ -20,35 +20,35 @@ module.exports = {
         // Watching
         const moviesJsonPath = path.resolve(__dirname, './../database/movies/names.json');
         const moviesData = require(moviesJsonPath);
-        const randomMovie = Math.floor(Math.random() * moviesData.length);
         // Games
         const gamesJsonPath = path.resolve(__dirname, './../database/games/names.json');
         const gamesData = require(gamesJsonPath);
-        const randomGame = Math.floor(Math.random() * gamesData.length);
         // Status
-        const status = [
-            // Custom
-            { activities: [{ name: 'I respond to DMs', type: ActivityType.Custom }], status: 'online' },
-            { activities: [{ name: '/help', type: ActivityType.Custom }], status: 'online' },
-            {
-                activities: [{
-                    // name is exposed through the API but not shown in the client for ActivityType.Custom
-                    name: 'custom',
-                    type: ActivityType.Custom,
-                    state: '🐥',
-                }],
-                status: 'online',
-            },
-            // Listening
-            { activities: [{ name: 'me', type: ActivityType.Listening }], status: 'idle' },
-            // Watching
-            { activities: [{ name: moviesData[randomMovie], type: ActivityType.Watching }], status: 'dnd' },
-            // Games
-            { activities: [{ name: gamesData[randomGame], type: ActivityType.Playing }], status: 'idle' },
-            { activities: [{ name: gamesData[randomGame], type: ActivityType.Competing }], status: 'dnd' },
-            { activities: [{ name: gamesData[randomGame], type: ActivityType.Streaming, url: 'https://www.twitch.tv/directory' }] },
-        ];
         function updateStatus() {
+            const randomMovie = Math.floor(Math.random() * moviesData.length);
+            const randomGame = Math.floor(Math.random() * gamesData.length);
+            const status = [
+                // Custom
+                { activities: [{ name: 'I respond to DMs', type: ActivityType.Custom }], status: 'online' },
+                { activities: [{ name: '/help', type: ActivityType.Custom }], status: 'online' },
+                {
+                    activities: [{
+                        // name is exposed through the API but not shown in the client for ActivityType.Custom
+                        name: 'custom',
+                        type: ActivityType.Custom,
+                        state: '🐥',
+                    }],
+                    status: 'online',
+                },
+                // Listening
+                { activities: [{ name: 'me', type: ActivityType.Listening }], status: 'idle' },
+                // Watching
+                { activities: [{ name: moviesData[randomMovie], type: ActivityType.Watching }], status: 'dnd' },
+                // Games
+                { activities: [{ name: gamesData[randomGame], type: ActivityType.Playing }], status: 'idle' },
+                { activities: [{ name: gamesData[randomGame], type: ActivityType.Competing }], status: 'dnd' },
+                { activities: [{ name: gamesData[randomGame], type: ActivityType.Streaming, url: 'https://www.twitch.tv/directory' }] },
+            ];
             const random = Math.floor(Math.random() * status.length);
             client.user.setPresence(status[random]);
         }
