b81fac1a4e91603fe70b464bd2e17b3bd6bf3671
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-25 13:06:51 +0100

    Update info.js [canary/commands/utility] (Embed Color)

--- canary/commands/utility/info.js
+++ canary/commands/utility/info.js
@@ -76,7 +76,6 @@ async function handleUserInfo(interaction) {
 
     // Create the embed with the user information
     const embed = new EmbedBuilder()
-        .setColor(embedColor)
         .setThumbnail(`${user.displayAvatarURL()}`)
         .addFields(
             { name: 'User:', value: `${user}`, inline: true },
@@ -85,29 +84,22 @@ async function handleUserInfo(interaction) {
             { name: 'ID:', value: `${user.id}`, inline: true },
             { name: 'Joined at:', value: `${member.joinedAt}`, inline: true },
         );
+    if (embedColor) embed.setColor(embedColor);
     await interaction.reply({ embeds: [embed], ephemeral: true });
 }
 
 function getEmbedColor(member) {
     const ROLES = {
-        pirate: '1205938469797625947',
-        captain: '1259986911460851783',
         serverBooster: '1207653983850594335',
-        treasurerOfTheNight: '1205938707442434118',
-        serverOwner: '1205588374354796574',
     };
 
     const ROLE_COLORS = {
-        [ROLES.pirate]: 0x71368A,
-        [ROLES.captain]: 0x9B59B6,
         [ROLES.serverBooster]: 0xF47FFF,
-        [ROLES.treasurerOfTheNight]: 0x010000,
-        [ROLES.serverOwner]: 0xF1C40F,
     };
 
     for (const [role, color] of Object.entries(ROLE_COLORS)) {
         if (member.roles.cache.has(role)) return color;
     }
 
-    return 'Default';
+    return null;
 }
