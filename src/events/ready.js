e9e4d9d88d6f2da04c8bb4e4dcfa37ec6d1bb447
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-09-07 18:29:24 +0100

    Update ready.js [src/events] (status)

--- src/events/ready.js
+++ src/events/ready.js
@@ -10,10 +10,10 @@ module.exports = {
             { activities: [{ name: 'I respond to DMs', type: ActivityType.Custom }], status: 'online' },
             { activities: [{ name: 'custom', type: ActivityType.Custom, state: 'Coding below the stars!' }], status: 'online' },
             { activities: [{ name: 'Music', type: ActivityType.Listening }], status: 'idle' },
-            { activities: [{ name: 'Movies and Series', type: ActivityType.Watching }], status: 'dnd' },
-            { activities: [{ name: 'Video Games', type: ActivityType.Playing }], status: 'idle' },
-            { activities: [{ name: 'Video Games', type: ActivityType.Competing }], status: 'dnd' },
-            { activities: [{ name: 'Video Games', type: ActivityType.Streaming, url: 'https://www.twitch.tv/directory' }] },
+            { activities: [{ name: 'Films', type: ActivityType.Watching }], status: 'dnd' },
+            { activities: [{ name: 'Games', type: ActivityType.Playing }], status: 'idle' },
+            { activities: [{ name: 'Games', type: ActivityType.Competing }], status: 'dnd' },
+            { activities: [{ name: 'Games', type: ActivityType.Streaming, url: 'https://www.twitch.tv/directory' }] },
         ];
         function updateStatus() {
             const random = Math.floor(Math.random() * status.length);
